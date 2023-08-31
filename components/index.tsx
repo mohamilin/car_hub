1783af4d69c1b040c1bc012fa58f794350831234
Moh Amilin <amilinmoh99@gmail.com>
2023-08-31 19:30:21 +0700

    add index

---
+++ components/index.tsx
@@ -0,0 +1,16 @@
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import CustomButton from './CustomButton'
+import SearchBar from './SearchBar'
+import CustomFilter from './CustomFilter'
+
+
+export {
+    Hero,
+    Navbar,
+    Footer,
+    CustomButton,
+    SearchBar,
+    CustomFilter
+}
