439a076b8746ae23f548ba3bdb2ec37e8240eb80
Moh Amilin <amilinmoh99@gmail.com>
2023-08-31 19:22:04 +0700

    add SearchManufacture component

---
+++ components/SearchManufacture.tsx
@@ -0,0 +1,95 @@
+import { useState, Fragment } from "react";
+import Image from "next/image";
+import { Combobox, Transition } from "@headlessui/react";
+import { CheckIcon } from '@heroicons/react/20/solid'
+
+import { SearchManufactureProps } from "@/types";
+import { manufacturers } from "@/contants";
+
+export default function SearchManufacture({
+  manufacture,
+  setManufacture,
+}: SearchManufactureProps) {
+  const [query, setQuery] = useState("");
+
+  const filterManufacturers =
+    query === ""
+      ? manufacturers
+      : manufacturers.filter((item) =>
+          item
+            .toLocaleLowerCase()
+            .replace(/\s+/g, "")
+            .includes(query.toLocaleLowerCase().replace(/\s+/g, ""))
+        );
+
+  return (
+    <div className="search-manufacturer">
+      <Combobox value={manufacture} onChange={setManufacture} >
+        <div className="relative w-full">
+          <Combobox.Button className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              width={20}
+              height={20}
+              alt="logo"
+              className="ml-4"
+            />
+          </Combobox.Button>
+          <Combobox.Input
+            className="search-manufacturer__input"
+            placeholder="Search Car"
+            displayValue={(manufacture: string) => manufacture}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+            afterLeave={() => setQuery("")}
+          >
+            <Combobox.Options>
+              {filterManufacturers.length === 0 && query !== "" ? (
+                <Combobox.Option
+                value={query}
+                className={"search-manufacturer__option"}
+                >
+                  Not Found `{query}`
+                </Combobox.Option>
+              ) : filterManufacturers.map((i,index) => (
+                <Combobox.Option
+                key={index}
+                className={({active}) => `relative search-manufacturer__option ${active ? 'bg-primary-blue text-White' : 'text-gray-900'} `}
+                value={i}
+                >
+                  
+                  {({ selected, active }) => (
+                      <>
+                        <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {i}
+                        </span>
+                        {selected ? (
+                          <span
+                            className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                              active ? 'text-white' : 'text-teal-600'
+                            }`}
+                          >
+                            <CheckIcon className="h-5 w-5" aria-hidden="true" />
+                          </span>
+                        ) : null}
+                      </>
+                    )}
+
+                </Combobox.Option>
+              ))}
+            </Combobox.Options>
+          </Transition>
+        </div>
+      </Combobox>
+    </div>
+  );
+}
