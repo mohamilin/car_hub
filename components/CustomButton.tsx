7e34e6298168d5949296b7d300b68202a4aca613
Moh Amilin <amilinmoh99@gmail.com>
2023-09-14 06:06:28 +0700

    add Component CustomFilter

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,22 +1,30 @@
 "use client";
 
 import { CustomButtonProps } from "@/types";
+import Image from "next/image";
 
 export default function CustomButton({
   title,
   btnType,
   containerStyles,
   handleClick,
+  textStyles,
+  rightIcon,
 }: CustomButtonProps) {
   return (
     <button
       disabled={false}
-      type={'button' || btnType}
+      type={"button" || btnType}
       // type={btnType}
       className={`custom-btn ${containerStyles} `}
       onClick={handleClick}
     >
-      <span className={`flex-1`}>{title}</span>
+      <span className={`flex-1 ${textStyles} `}>{title}</span>
+      {rightIcon && (
+        <div className="relative w-6 h-6">
+          <Image src={rightIcon} alt="icon" fill className="object-contain" />
+        </div>
+      )}
     </button>
   );
 }
