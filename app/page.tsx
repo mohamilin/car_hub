3996f6ada9a001a4128efffd4ad9989b9ce78837
Moh Amilin <amilinmoh99@gmail.com>
2023-09-14 06:08:24 +0700

    update page

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,30 @@
 
 import { useEffect, useState } from "react";
 
-import { SearchBar, CustomFilter, Hero } from "@/components";
+import { SearchBar, CustomFilter, Hero, CardCar } from "@/components";
 import { fetchCars } from "@/utils";
 
 export default function Home() {
   const [cars, setCars] = useState([]);
-
+  const [search, setSearch] = useState("corolla");
+  const [messageError, setMessageError] = useState("");
   useEffect(() => {
-    let params = "corolla";
-    fetchCars(params).then((res) => {
-      console.log(res);
-      setCars(res)
-    });
+    let params = "bmw";
+    fetchCars(params)
+      .then((res) => {
+        console.log(res);
+        setCars(res);
+      })
+      .catch((e) => {
+        console.log(
+          e.response.data.info,
+          "https://meliasehatsejahtera.biz.id/#"
+        );
+        setMessageError(e.response.data.info);
+      });
   }, []);
+
+  const dataIsEmpty = !Array.isArray(cars) || cars.length < 1 || !cars;
 
   return (
     <main className="overflow-hidden">
@@ -29,10 +40,31 @@ export default function Home() {
         <div className="home__filters">
           <SearchBar />
           <div className="home__filter-container">
-            <CustomFilter />
-            <CustomFilter />
+            <CustomFilter title="Fuel" />
+            <CustomFilter title="Year" />
           </div>
         </div>
+
+        {!dataIsEmpty && !messageError ? (
+          <section>
+            <div className="home__cars-wrapper">
+              {cars?.map((item, index) => (
+                <CardCar car={item} key={index} />
+              ))}
+            </div>
+          </section>
+        ) : (
+          <div className="home__error-container">
+            {messageError ? (
+              <h2 className="text-black text-xl font-bold">
+                SERVER ERROR <br/>
+                <span>{messageError}</span>
+              </h2>
+            ) : (
+              <h2 className="text-black text-xl font-bold">Loding ...</h2>
+            )}
+          </div>
+        )}
       </div>
     </main>
   );
