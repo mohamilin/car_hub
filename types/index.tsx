5bd7a6fde592e80c594befb2641e8f7afbe234cb
Moh Amilin <amilinmoh99@gmail.com>
2023-09-14 06:07:13 +0700

    add types

--- types/index.tsx
+++ types/index.tsx
@@ -1,14 +1,30 @@
 import { MouseEventHandler } from "react";
 
 export interface CustomButtonProps {
-    title: string,
-    containerStyles?: string,
-    btnType?: 'button' | 'submit',
-    handleClick?: MouseEventHandler<HTMLButtonElement>
+  title: string;
+  containerStyles?: string;
+  btnType?: "button" | "submit";
+  textStyles?: string;
+  rightIcon?: string,
+  handleClick?: MouseEventHandler<HTMLButtonElement>;
 }
 
-
 export interface SearchManufactureProps {
-    manufacture: string,
-    setManufacture: (manufacture:string) => void
+  manufacture: string;
+  setManufacture: (manufacture: string) => void;
+}
+
+export interface CardCarProps {
+  city_mpg: number;
+  class: string;
+  combination_mpg: number;
+  cylinders: number;
+  displacement: number;
+  drive: string;
+  fuel_type: string;
+  highway_mpg: number;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
 }
